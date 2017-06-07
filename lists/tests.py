20c85266417448c7d74f7eba1b5d06cee52f2ac6
Vincent D'Onofrio <vincent.donofrio@soul-cycle.com>
2017-06-07 15:49:21 -0400

    Front page HTML now generated from a template

--- lists/tests.py
+++ lists/tests.py
@@ -2,6 +2,6 @@ from django.test import TestCase
 
 class HomePageTest(TestCase):
 
-    def test_home_page_returns_correct_html(self):
+    def test_uses_home_template(self):
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'home.html')
